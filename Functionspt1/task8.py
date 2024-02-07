89431f738d28afba884e860572715f4b1df990a9
Albicocca224 <98885281+Albicocca224@users.noreply.github.com>
2024-02-07 22:28:21 +0600

    Update task8.py

--- Functionspt1/task8.py
+++ Functionspt1/task8.py
@@ -1,14 +1,16 @@
 def spy_game(nums):
-    zero_found = False
-    seven_found = False
+    found_0 = False
+    found_00 = False
     for num in nums:
         if num == 0:
-            zero_found = True
-        elif num == 7 and zero_found:
-            seven_found = True
-        if zero_found and seven_found:
-            return True
+            if found_0:
+                found_00 = True
+            else:
+                found_0 = True
+        elif num == 7:
+            if found_0 and found_00:
+                return True
     return False
-print(spy_game([1,2,4,0,0,7,5]))
-print(spy_game([1,0,2,4,0,5,7]))
-print(spy_game([1,7,2,0,4,5,0]))  
+print(spy_game([1,2,4,0,0,7,5]))  
+print(spy_game([1,0,2,4,0,5,7])) 
+print(spy_game([1,7,2,0,4,7,0]))  
